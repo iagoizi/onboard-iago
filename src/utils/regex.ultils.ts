c96ad994e8dda432992ce4167d9e5be41dd04494
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-07-14 17:54:30 -0300

    Modificando nome das constantes

--- src/utils/regex.ultils.ts
+++ src/utils/regex.ultils.ts
@@ -1,2 +1,2 @@
-export const emailRegex = 'w+[+.w-]*@([w-]+.)*w+[w-]*.([a-zA-Z]{2,}|d+)'; //consultei https://www.formget.com/regular-expression-for-email/
-export const passwordRegex = '(?=.*[a-zA-Z])(?=.*d).{7,}';
+export const EMAIL_REGEX = '\\w+[\\+\\.\\w-]*@([\\w-]+\\.)*\\w+[\\w-]*.([a-zA-Z]{2,}|\\d+)'; //consultei https://www.formget.com/regular-expression-for-email/
+export const PASSWORD_REGEX = '(?=.*[a-zA-Z])(?=.*\\d).{7,}';
