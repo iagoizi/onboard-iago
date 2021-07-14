7fe4ebc27675be3f8316fbd7e3e6bbbfc0377944
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-07-14 18:04:56 -0300

    Criando mutation e provider

---
+++ src/data/loginMutation.ts
@@ -0,0 +1,30 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_MUTATION = gql`
+  mutation Login($data: LoginInputType!) {
+    login(data: $data) {
+      user {
+        id
+        email
+        name
+        phone
+        role
+        phone
+      }
+      token
+    }
+  }
+`;
+
+export interface UserData {
+  name: string;
+  email: string;
+  role: string;
+  phone: string;
+  key: any;
+}
+
+export interface LoginData {
+  token: string;
+  user: UserData;
+}
