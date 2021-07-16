d10497a4a9f1c1fbb8a5d29398307415285478bd
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-07-16 04:53:15 -0300

    Integração da lista de usuários com a api

--- src/modules/user-list.tsx
+++ src/modules/user-list.tsx
@@ -1,15 +1,34 @@
+import { useQuery } from '@apollo/client';
 import React from 'react';
-import { USERS } from '../assets/fake-data';
 import { Cell } from '../components/cell';
+import { UsersListData, USERS_QUERY } from '../data/users-query';
 
 export const UserList: React.FC = () => {
-  const list = USERS.map((user) => (
-    <Cell key={user.email}>
+  const token = localStorage.getItem('token');
+  const { data, error } = useQuery<UsersListData>(USERS_QUERY, {
+    context: {
+      headers: {
+        Authorization: token,
+      },
+    },
+    variables: {
+      offset: 0,
+      limit: 10,
+    },
+  });
+
+  const list = data?.users.nodes.map((user) => (
+    <Cell key={user.id}>
       <p>
         {user.name} : {user.email}
       </p>
     </Cell>
   ));
 
-  return <h1>{list}</h1>;
+  return (
+    <div>
+      {error && <span style={{ color: 'red' }}>{error.message}</span>}
+      {list}
+    </div>
+  );
 };
