3ae4e79481a1a92bd325f5537149bf4b5abc715e
Iago Izidório Lacerda <iagoilacerda@gmail.com>
2021-07-18 04:35:25 -0300

    Colocando rotas como constantes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { AddUser } from './modules/add-user';
 import { LoginScreen } from './modules/login';
 import { NotFound } from './modules/not-found';
 import { UserList } from './modules/user-list';
+import { ADD_USER_PATH, LOGIN_PATH, USERS_PATH } from './utils/routes';
 
 export const App: React.FC = () => {
   return (
     <div>
       <BrowserRouter>
         <Switch>
-          <Route path='/login' component={LoginScreen} />
-          <Route path='/user-list/:page' component={UserList} />
+          <Route path={LOGIN_PATH} component={LoginScreen} />
+          <Route path={`${USERS_PATH}/:page`} component={UserList} />
+          <Route path={ADD_USER_PATH} component={AddUser} />
           <Route path='/' component={NotFound} />
         </Switch>
       </BrowserRouter>
